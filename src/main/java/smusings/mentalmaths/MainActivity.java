5d9762a3f42700d5282cf40e9576161d91b7d6b7
smusings <etyulmenkov@yahoo.com>
2014-09-13 16:54:10 -0400

    dont like { on new line

--- src/main/java/smusings/mentalmaths/MainActivity.java
+++ src/main/java/smusings/mentalmaths/MainActivity.java
@@ -12,11 +12,11 @@ import android.widget.TextView;
 public class MainActivity extends SetupActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //defines everythign we need for this application
         num1 = (TextView) findViewById(R.id.num1);
         num2 = (TextView) findViewById(R.id.num2);
         countRight = (TextView) findViewById(R.id.count_right);
@@ -27,12 +27,14 @@ public class MainActivity extends SetupActivity {
         multiplicandLayout = (LinearLayout) findViewById(R.id.multiplicant_layout);
         multiplierLayout = (LinearLayout) findViewById(R.id.multiplier_layout);
 
+        //initial setup
         countRight.setText("0");
         multiplicantSeek.setProgress(0);
         multiplierSeek.setProgress(0);
         seekBar_1_call();
         seekBar_2_call();
 
+        //assigns listeners to interactable elements
         multiplicantSeek.setOnSeekBarChangeListener(seeker1);
         multiplierSeek.setOnSeekBarChangeListener(seeker2);
 
@@ -41,22 +43,19 @@ public class MainActivity extends SetupActivity {
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
+    public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
+    public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings)
-        {
+        if (id == R.id.action_settings) {
             return true;
         }
         return super.onOptionsItemSelected(item);
--- src/main/java/smusings/mentalmaths/SetupActivity.java
+++ src/main/java/smusings/mentalmaths/SetupActivity.java
@@ -27,8 +27,8 @@ public class SetupActivity extends Activity {
     public LinearLayout multiplierLayout;
 
 
-    public static int numSetUp(int min, int max)
-    {
+    //gives us our pseudo-random numbers
+    public static int numSetUp(int min, int max) {
         Random random = new Random();
 
         int randomInt=random.nextInt(max - min) + min;
@@ -36,98 +36,79 @@ public class SetupActivity extends Activity {
         return randomInt;
     }
 
-    SeekBar.OnSeekBarChangeListener seeker1 = new SeekBar.OnSeekBarChangeListener()
-    {
+    //a listener for seekbar
+    SeekBar.OnSeekBarChangeListener seeker1 = new SeekBar.OnSeekBarChangeListener() {
         int progressOne = 0;
 
+        //if progress changes we do one of four things
         @Override
-        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
-        {
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
             progressOne = progress;
 
-            if (progressOne == 0)
-            {
+            if (progressOne == 0) {
                 num1.setText(Integer.toString(numSetUp(1, 9)));
             }
-            else if (progressOne == 1)
-            {
+            else if (progressOne == 1) {
                 num1.setText(Integer.toString(numSetUp(10, 99)));
             }
-            else if (progressOne == 2)
-            {
+            else if (progressOne == 2) {
                 num1.setText(Integer.toString(numSetUp(100, 999)));
             }
-            else if (progressOne == 3)
-            {
+            else if (progressOne == 3) {
                 num1.setText(Integer.toString(numSetUp(100, 9999)));
             }
         }
 
         @Override
-        public void onStartTrackingTouch(SeekBar seekBar)
-        {
-
+        public void onStartTrackingTouch(SeekBar seekBar) {
+            //TODO: Auto-generated
         }
 
         @Override
-        public void onStopTrackingTouch(SeekBar seekBar)
-        {
-
-
+        public void onStopTrackingTouch(SeekBar seekBar) {
+            //TODO: Auto-generated
         }
     };
 
-    SeekBar.OnSeekBarChangeListener seeker2 = new SeekBar.OnSeekBarChangeListener()
-    {
+    SeekBar.OnSeekBarChangeListener seeker2 = new SeekBar.OnSeekBarChangeListener() {
         int progressTwo = 0;
 
         @Override
-        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
-        {
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
             progressTwo = progress;
 
-            if (progressTwo == 0)
-            {
+            if (progressTwo == 0) {
                 num2.setText(Integer.toString(numSetUp(1, 9)));
             }
-            else if (progressTwo == 1)
-            {
+            else if (progressTwo == 1) {
                 num2.setText(Integer.toString(numSetUp(10, 99)));
             }
-            else if (progressTwo == 2)
-            {
+            else if (progressTwo == 2) {
                 num2.setText(Integer.toString(numSetUp(100, 999)));
             }
-            else if (progressTwo == 3)
-            {
+            else if (progressTwo == 3) {
                 num2.setText(Integer.toString(numSetUp(100, 9999)));
             }
         }
 
         @Override
-        public void onStartTrackingTouch(SeekBar seekBar)
-        {
-
+        public void onStartTrackingTouch(SeekBar seekBar) {
+            //TODO: Auto-generated
         }
 
         @Override
-        public void onStopTrackingTouch(SeekBar seekBar)
-        {
-
-
+        public void onStopTrackingTouch(SeekBar seekBar) {
+            //TODO: Auto-generated
         }
     };
 
-    TextView.OnEditorActionListener result_entered = new TextView.OnEditorActionListener()
-    {
+    TextView.OnEditorActionListener result_entered = new TextView.OnEditorActionListener() {
         @Override
-        public boolean onEditorAction(TextView v, int actionId, KeyEvent event)
-        {
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
             if ((actionId == EditorInfo.IME_ACTION_DONE) ||
                     ((event.isShiftPressed() == false)) &&
                             (event.getKeyCode() == KeyEvent.KEYCODE_ENTER) &&
-                            (event.getAction() == KeyEvent.ACTION_DOWN))
-            {
+                            (event.getAction() == KeyEvent.ACTION_DOWN)) {
 
 
                 multiplicandLayout.setVisibility(View.GONE);
@@ -137,13 +118,12 @@ public class SetupActivity extends Activity {
                 int result=Integer.valueOf(num1.getText().toString()) *
                         Integer.valueOf(num2.getText().toString());
 
-                if (num_answer.getText().toString().matches(""))
-                {
+                if (num_answer.getText().toString().matches("")) {
                     num_answer.setText("");
                     num_answer.clearFocus();
                 }
-                else if (Integer.valueOf(num_answer.getText().toString()) == result)
-                {
+
+                else if (Integer.valueOf(num_answer.getText().toString()) == result) {
                     Toast.makeText(SetupActivity.this, "Correct!", Toast.LENGTH_SHORT).show();
                     num_answer.setText("");
                     setCountPlusOne();
@@ -151,8 +131,8 @@ public class SetupActivity extends Activity {
                     seekBar_2_call();
                     cdt.start();
                 }
-                else if (Integer.valueOf(num_answer.getText().toString()) != result)
-                {
+
+                else if (Integer.valueOf(num_answer.getText().toString()) != result) {
                     Toast.makeText(SetupActivity.this, "Wrong!", Toast.LENGTH_SHORT).show();
                     num_answer.setText("");
                 }
@@ -162,56 +142,44 @@ public class SetupActivity extends Activity {
     };
 
 
-    public void seekBar_1_call()
-    {
-        if (multiplicantSeek.getProgress() == 0)
-        {
+    public void seekBar_1_call() {
+        if (multiplicantSeek.getProgress() == 0) {
             num1.setText(Integer.toString(numSetUp(1, 9)));
         }
-        else if (multiplicantSeek.getProgress() == 1)
-        {
+        else if (multiplicantSeek.getProgress() == 1) {
             num1.setText(Integer.toString(numSetUp(10, 99)));
         }
-        else if (multiplicantSeek.getProgress() == 2)
-        {
+        else if (multiplicantSeek.getProgress() == 2) {
             num1.setText(Integer.toString(numSetUp(100, 999)));
         }
-        else if (multiplicantSeek.getProgress() == 3)
-        {
+        else if (multiplicantSeek.getProgress() == 3) {
             num1.setText(Integer.toString(numSetUp(1000, 9999)));
         }
     }
 
-    public void seekBar_2_call()
-    {
-        if (multiplierSeek.getProgress() == 0)
-        {
+    public void seekBar_2_call() {
+        if (multiplierSeek.getProgress() == 0) {
             num2.setText(Integer.toString(numSetUp(1, 9)));
         }
-        else if (multiplierSeek.getProgress() == 1)
-        {
+        else if (multiplierSeek.getProgress() == 1) {
             num2.setText(Integer.toString(numSetUp(10, 99)));
         }
-        else if (multiplierSeek.getProgress() == 2)
-        {
+        else if (multiplierSeek.getProgress() == 2) {
             num2.setText(Integer.toString(numSetUp(100, 999)));
         }
-        else if (multiplierSeek.getProgress() == 3)
-        {
+        else if (multiplierSeek.getProgress() == 3) {
             num2.setText(Integer.toString(numSetUp(1000, 9999)));
         }
     }
 
     CountDownTimer cdt = new CountDownTimer(30000, 1000) {
         @Override
-        public void onTick(long millisUntilFinished)
-        {
+        public void onTick(long millisUntilFinished) {
             timer.setText("Seconds Left: " + millisUntilFinished / 1000);
         }
 
         @Override
-        public void onFinish()
-        {
+        public void onFinish() {
             timer.setText("0");
             multiplicandLayout.setVisibility(View.VISIBLE);
             multiplierLayout.setVisibility(View.VISIBLE);
@@ -221,8 +189,7 @@ public class SetupActivity extends Activity {
         }
     };
 
-    public void setCountPlusOne()
-    {
+    public void setCountPlusOne() {
         int oldCount = Integer.valueOf(countRight.getText().toString());
         int newCount = oldCount + 1;
         countRight.setText(Integer.toString(newCount));
